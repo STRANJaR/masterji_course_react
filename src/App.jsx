2f0e0e296767682ebf47a9cb05ec3374d3087a1e
Rohit Shrivastav <rohitrks805@gmail.com>
2024-07-22 21:10:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='bg-gray-800'>
+      <h1 className="text-3xl font-bold text-center underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
