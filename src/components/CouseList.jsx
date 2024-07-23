2cd5f7b89bcb1876c74f21fa3e15ed920d0c9843
Rohit Shrivastav <rohitrks805@gmail.com>
2024-07-24 00:06:51 +0530

    dnd fixed

--- src/components/CouseList.jsx
+++ src/components/CouseList.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
 import branding from '../assets/branding.png'
-import course1 from '../assets/course1.png'
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import courseDataJSON from '../course.json'
 import { useState } from "react";
+// import course1 from '../assets/course1.png'
 
 function CouseList() {
 
@@ -11,9 +12,15 @@ function CouseList() {
     useState(()=> {
         setCourseData(courseDataJSON)
     })
+
+    const handleDragEnd = (result) => {
+        //
+    }
   return (
+    <DragDropContext onDragEnd={handleDragEnd}>
+
     <main className="bg-[#D2E3C8] text-black h-screen w-full">
-        <h1 className="text-4xl font-extrabold text-center py-5">Chai aur Code</h1>
+        <h1 className=" text-[#4F6F52] text-4xl font-extrabold text-center py-5">Chai aur Code</h1>
 
         {/* Branding Logo  */}
         <Link to={'https://chaicode.com/'} target='_blank'>
@@ -26,44 +33,67 @@ function CouseList() {
         </Link>
         
 
-        <div className="bg-[#F9F7F7] min-h-3.5 w-2/3 ml-5 p-4 rounded-lg"> 
-            <div>
-                <h1 className="text-2xl text-[#313131]">Manage Bundle</h1>
-                <p className="text-sm text-[#4B4747]">Change orders of the products based on priorty</p>
-            </div>
+            <div className="bg-[#F9F7F7] min-h-3.5 w-2/3 ml-5 p-4 rounded-lg"> 
+                    <div>
+                        <h1 className="text-2xl font-bold text-[#313131]">Manage Bundle</h1>
+                        <p className="text-sm text-[#4B4747]">Change orders of the products based on priorty</p>
+                    </div>
 
-            {courseData && courseData.map((course)=> (
-                <div 
-                key={course.id}
-                className="bg-[#F7F7F7] shadow-[10px_10px_60px_-15px_rgba(0,0,0,0.3)] border border-blue-200  my-3 flex flex-row justify-between w-5/6 h-16 p-3 items-center rounded-md">
+                <div className="">
+                    <Droppable droppableId="course">
+                        {(provided)=> (
+                            <div ref={provided.innerRef} {...provided.droppableProps}>
 
-                <div className=" pl-3 h-auto w-auto flex flex-row items-center gap-4 text-sm">
-                    <img 
-                    className="w-24 rounded-lg "
-                    src= {course.picture} 
-                    alt="courseLogo" 
-                    />
 
-                    <h1 className="">{course.title}</h1>
-                </div>
+                                {courseData && courseData.map(({id, title, price, type, picture}, index)=> (
 
-                <div className="flex flex-row gap-3 text-sm">
-                    <p>{course.price}</p>
-                    <p
-                    className=" text-center text-sm w-[82px] h-[26] bg-[#DBFFCE] rounded-sm border border-[#7e7e7e46]"
-                    >{course.type}</p>
-                </div>
-            </div>
+                                    <Draggable
+                                    key={id}
+                                    draggableId={id.toString()}
+                                    index={index}
+                                    >
+                                    {(provided)=> (
+                                    <div 
+                                        ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}
+                                        // key={course.id}
+                                        className="bg-[#F7F7F7] shadow-[10px_10px_60px_-15px_rgba(0,0,0,0.3)] border border-blue-200  my-3 flex flex-row justify-between w-5/6 h-16 px-3 items-center rounded-md">
             
-            ))
-
-            }
+                                        <div className=" h-auto w-auto flex flex-row items-center text-sm">
+                                        <svg width="40px" height="137px" viewBox="-2.5 -2.5 30.00 30.00" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#696969" strokeWidth="0.00025" transform="matrix(1, 0, 0, 1, 0, 0)rotate(0)"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#CCCCCC" strokeWidth="0.2"></g><g id="SVGRepo_iconCarrier"> <path fillRule="evenodd" clipRule="evenodd" d="M9.5 8C10.3284 8 11 7.32843 11 6.5C11 5.67157 10.3284 5 9.5 5C8.67157 5 8 5.67157 8 6.5C8 7.32843 8.67157 8 9.5 8ZM9.5 14C10.3284 14 11 13.3284 11 12.5C11 11.6716 10.3284 11 9.5 11C8.67157 11 8 11.6716 8 12.5C8 13.3284 8.67157 14 9.5 14ZM11 18.5C11 19.3284 10.3284 20 9.5 20C8.67157 20 8 19.3284 8 18.5C8 17.6716 8.67157 17 9.5 17C10.3284 17 11 17.6716 11 18.5ZM15.5 8C16.3284 8 17 7.32843 17 6.5C17 5.67157 16.3284 5 15.5 5C14.6716 5 14 5.67157 14 6.5C14 7.32843 14.6716 8 15.5 8ZM17 12.5C17 13.3284 16.3284 14 15.5 14C14.6716 14 14 13.3284 14 12.5C14 11.6716 14.6716 11 15.5 11C16.3284 11 17 11.6716 17 12.5ZM15.5 20C16.3284 20 17 19.3284 17 18.5C17 17.6716 16.3284 17 15.5 17C14.6716 17 14 17.6716 14 18.5C14 19.3284 14.6716 20 15.5 20Z" fill="#474747"></path> </g></svg>
+                                            <img 
+                                            className="w-24 rounded-lg "
+                                            src= {picture} 
+                                            alt="courseLogo" 
+                                            />
             
-           
+                                        <h1 className="pl-4">{title}</h1>
+                                    </div>
+            
+                                    <div className="flex flex-row gap-3 text-sm">
+                                        <p>{price}</p>
+                                        <p
+                                        className=" text-center text-sm w-[82px] h-[26] bg-[#DBFFCE] rounded-sm border border-[#7e7e7e46]"
+                                        >{type}</p>
+                                    </div>
+                                    </div>
+                                    )}
+                                    </Draggable>
+                                
+                                ))
+                                }
+                                {provided.placeholder}
+                            </div>
 
-        </div>
+                        )}
+                    </Droppable>
+                </div>
+                    
+                    
 
+            </div>
     </main>
+    </DragDropContext>
+
   )
 }
 
